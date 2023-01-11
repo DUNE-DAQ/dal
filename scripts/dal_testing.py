ae944a63e483bb8b5ab1b2aa9397fa69fc675c94
John Freeman <jcfree@mu2edaq13.fnal.gov>
2023-01-11 16:04:15 -0600

    JCF: remove hardwired path to core.schema.xml in dal_testing.data.xml and minor touch-ups to dal_testing.py

--- scripts/dal_testing.py
+++ scripts/dal_testing.py
@@ -3,21 +3,20 @@
 import config
 import dal
 import os
-#from config._daq_config_py import _Configuration
 
 # MUST IMPROVE PATH FINDING!
 scriptsdir=os.path.dirname(os.path.realpath(__file__))
-db = config.Configuration(f"oksconfig:{scriptsdir}/dal_testing.data.xml")
 
-# #apps = dal.partition_get_all_applications(db, "JohnsPartition")
+print(f"\nReading in {scriptsdir}/dal_testing.data.xml:\n")
+db = config.Configuration(f"oksconfig:{scriptsdir}/dal_testing.data.xml")
 
 partition = db.get_dal("Partition", "JohnsPartition")
 
 apps = partition.get_all_applications(db, "JohnsPartition")
-# print(len(apps))
 
 
-print("")
 for app in apps:
     print(app.get_app_id())
+
+print("")
 
